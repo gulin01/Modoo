f2963fd8f7ae55573b8e591b294b7531fd57d3a3
guli9717 <javohir.manchester@gmail.com>
2024-11-01 03:19:56 +0900

    inquiry modal

---
+++ public/js/inquiry.js
@@ -0,0 +1,7 @@
+const modal = document.getElementById('inquiry-modal')
+const inquiryBtn = document.getElementById('inquiry-btn')
+
+inquiryBtn.onclick = () => {
+    modal.style.display = 'block'
+}
+
